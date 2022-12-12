d7f14205fa1521eb02bba4afac38c6c719298822
davidkhachatryan123 <xdavit777@icloud.com>
2022-12-12 21:25:02 +0400

    21:24 12-Dec-22

--- David_Studio_Server/Controllers/Admin/Dashboard/Home/HomeServices.cs
+++ David_Studio_Server/Controllers/Admin/Dashboard/Home/HomeServices.cs
@@ -1,9 +1,11 @@
-﻿using David_Studio_Server.Database.Models.Content.Services;
+﻿using David_Studio_Server.Database;
+using David_Studio_Server.Database.Models.Content.Services;
 using David_Studio_Server.Models;
 using David_Studio_Server.Models.Dashboard.Home;
 using David_Studio_Server.Services.DB.Home;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace David_Studio_Server.Controllers.Admin.Dashboard.Home
 {
@@ -13,10 +15,14 @@ namespace David_Studio_Server.Controllers.Admin.Dashboard.Home
     public class HomeServices : ControllerBase
     {
         private readonly IHomeServicesDataProvider _homeServices;
+        private readonly DavidStudioContext _context;
 
-        public HomeServices(IHomeServicesDataProvider homeServices)
+        public HomeServices(
+            IHomeServicesDataProvider homeServices,
+            DavidStudioContext context)
         {
             _homeServices = homeServices;
+            _context = context;
         }
 
         [Route("services")]
@@ -27,11 +33,21 @@ namespace David_Studio_Server.Controllers.Admin.Dashboard.Home
         }
 
         [HttpPost]
-        public async Task<ResponseModel> DataUpdate([FromBody] HomeServiceData homeServiceData)
+        public async Task<ResponseModel> Post([FromBody] HomeServiceData homeServiceData)
         {
-            var result = await _homeServices.UpdateHomeServiceAsync(homeServiceData);
+            bool result = false;
 
-            return new ResponseModel("", StatusCodes.Status200OK);
+            HomeService? homeService = await _context.HomeServices
+                .FirstOrDefaultAsync(x => x.Id == homeServiceData.HomeServiceId);
+
+            if (homeService == null)
+                result = await _homeServices.AddNewHomeServiceAsync(homeServiceData);
+            else
+            {
+                result = await _homeServices.UpdateHomeServiceAsync(homeServiceData);
+            }
+
+            return new ResponseModel(result.ToString(), StatusCodes.Status200OK);
         }
     }
 }
--- David_Studio_Server/Services/DB/Home/HomeServicesDataProvider.cs
+++ David_Studio_Server/Services/DB/Home/HomeServicesDataProvider.cs
@@ -1,16 +1,17 @@
-﻿using David_Studio_Server.Controllers.Admin.Dashboard.Home;
-using David_Studio_Server.Database;
+﻿using David_Studio_Server.Database;
 using David_Studio_Server.Database.Models.Content.Services;
 using David_Studio_Server.Database.Models.Content.Translation;
 using David_Studio_Server.Models.Dashboard.Home;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace David_Studio_Server.Services.DB.Home
 {
     public interface IHomeServicesDataProvider
     {
         Task<IEnumerable<Service>> GetServicesAsync();
-        Task<HomeService> UpdateHomeServiceAsync(HomeServiceData homeServicedata);
+        Task<bool> AddNewHomeServiceAsync(HomeServiceData homeServiceData);
+        Task<bool> UpdateHomeServiceAsync(HomeServiceData homeServiceData);
     }
 
     public class HomeServicesDataProvider : IHomeServicesDataProvider
@@ -28,69 +29,81 @@ namespace David_Studio_Server.Services.DB.Home
                 .ToArrayAsync();
         }
 
-        public async Task<HomeService> UpdateHomeServiceAsync(HomeServiceData homeServicedata)
+        public async Task<bool> AddNewHomeServiceAsync(HomeServiceData homeServiceData)
         {
-            HomeService? homeService = await _context.HomeServices
-                .FirstOrDefaultAsync(x => x.Id == homeServicedata.HomeServiceId);
+            using var transaction = _context.Database.BeginTransaction();
 
-            if (homeService == null)
+            try
             {
-                await AddNewHomeServiceAsync(homeServicedata);
+                Service? service = await _context.Services.FirstOrDefaultAsync(x => x.Id == homeServiceData.ServiceId);
+                Language? lang = await _context.Languages.FirstOrDefaultAsync(x => x.Id == homeServiceData.LanguageId);
+
+                if (service != null && lang != null)
+                {
+                    HomeService homeService = new()
+                    {
+                        ServiceId = service.Id,
+                        ImageId = homeServiceData.ImageId,
+                        ButtonColor = homeServiceData.ButtonColor
+                    };
+
+                    await _context.HomeServices.AddAsync(homeService);
+                    await _context.SaveChangesAsync();
+
+
+                    Translation title = new()
+                    {
+                        Language = lang,
+                        Text = homeServiceData.Title
+                    },
+                    description = new()
+                    {
+                        Language = lang,
+                        Text = homeServiceData.Description
+                    };
+
+                    await _context.Translations.AddRangeAsync(title, description);
+                    await _context.SaveChangesAsync();
+
+
+                    HomeServiceTranslation homeServiceTranslation = new()
+                    {
+                        HomeServiceId = homeService.Id,
+                        TitleTranslationId = title.Id,
+                        DescriptionTranslationId = description.Id
+                    };
+
+                    await _context.HomeServiceTranslations.AddAsync(homeServiceTranslation);
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+                }
             }
-            else
+            catch
             {
-
+                return false;
             }
 
-            await _context.SaveChangesAsync();
-
-            return homeService;
+            return true;
         }
 
-        private async Task<bool> AddNewHomeServiceAsync(HomeServiceData homeServicedata)
+        public async Task<bool> UpdateHomeServiceAsync(HomeServiceData homeServiceData)
         {
-            Service? service = await _context.Services.FirstOrDefaultAsync(x => x.Id == homeServicedata.ServiceId);
-            Language? lang = await _context.Languages.FirstOrDefaultAsync(x => x.Id == homeServicedata.LanguageId);
+            Translation? title = await _context.Translations
+                .Include(x => x.Language)
+                .Include(x => x.ServiceTitleTranslations.Where(y => y.HomeServiceId == homeServiceData.HomeServiceId))
+                .Where(x => x.Language.Id == homeServiceData.LanguageId)
+                .Where(x => x.ServiceTitleTranslations.Any())
+                .FirstOrDefaultAsync();
 
-            if (service != null && lang != null)
-            {
-                HomeService homeService = new()
-                {
-                    ServiceId = service.Id,
-                    ImageId = homeServicedata.ImageId,
-                    ButtonColor = homeServicedata.ButtonColor
-                };
-
-                await _context.HomeServices.AddAsync(homeService);
-                await _context.SaveChangesAsync();
-
-
-                Translation title = new()
-                {
-                    Language = lang,
-                    Text = homeServicedata.Title
-                },
-                description = new()
-                {
-                    Language = lang,
-                    Text = homeServicedata.Description
-                };
-
-                await _context.Translations.AddRangeAsync(title, description);
-                await _context.SaveChangesAsync();
+            Translation? description = await _context.Translations
+                .Include(x => x.Language)
+                .Include(x => x.ServiceDescriptionTranslations.Where(y => y.HomeServiceId == homeServiceData.HomeServiceId))
+                .Where(x => x.Language.Id == homeServiceData.LanguageId)
+                .Where(x => x.ServiceDescriptionTranslations.Any())
+                .FirstOrDefaultAsync();
 
 
-                HomeServiceTranslation homeServiceTranslation = new()
-                {
-                    HomeServiceId = homeService.Id,
-                    TitleTranslationId = title.Id,
-                    DescriptionTranslationId = description.Id
-                };
-
-                await _context.HomeServiceTranslations.AddAsync(homeServiceTranslation);
-                await _context.SaveChangesAsync();
-            }
-            else return false;
 
             return true;
         }
